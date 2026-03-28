9003a461c5233c0b2e226a91eb029086935072f9
Alex Plotnikov <applotnikov@edu.hse.ru>
2026-03-28 18:40:56 +0300

    Inference on cuda

--- inference.py
+++ inference.py
@@ -1,13 +1,15 @@
 import os
+import torch
 from diffusers import StableDiffusion3Pipeline
 
 hf_token = os.environ.get("HF_TOKEN")
 
-pipe = StableDiffusion3Pipeline.from_pretrained(
+model = StableDiffusion3Pipeline.from_pretrained(
     "stabilityai/stable-diffusion-3.5-medium",
-    token=hf_token
-)
+    torch_dtype=torch.float16
+).to("cuda")
+
 
 def inference(prompt):
-    image = pipe(prompt).images[0]
+    image = model(prompt).images[0]
     return image
