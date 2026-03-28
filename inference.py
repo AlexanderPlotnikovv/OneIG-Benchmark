7a4634673214de2bc9d461180a7afa4b34336f80
Alex Plotnikov <applotnikov@edu.hse.ru>
2026-03-28 18:27:12 +0300

    Created env interaction

--- inference.py
+++ inference.py
@@ -1,10 +1,13 @@
+import os
 from diffusers import StableDiffusion3Pipeline
 
-model = StableDiffusion3Pipeline.from_pretrained(
-    "stabilityai/stable-diffusion-3-medium-diffusers",
+hf_token = os.environ.get("HF_TOKEN")
+
+pipe = StableDiffusion3Pipeline.from_pretrained(
+    "stabilityai/stable-diffusion-3.5-medium",
     token=hf_token
 )
 
 def inference(prompt):
-    image = model(prompt).images[0]
+    image = pipe(prompt).images[0]
     return image
